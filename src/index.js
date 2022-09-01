587e14cd356adf223654aa2a0c7f2afaa7103774
schedrov <rocolusso@icloud.com>
2022-09-01 13:32:46 +0300

    added Progress component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Progress from './Progress';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Progress percentage={40} />
+  </React.StrictMode>,
 );
